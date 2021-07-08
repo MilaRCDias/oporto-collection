e2156efd57c1eecba7cdb55e7dd12cc5009ec7bd
mila <milarabello.dias@gmail.com>
2021-07-08 14:47:30 +0100

    changed url to book now

--- data/apartments.js
+++ data/apartments.js
@@ -12,7 +12,7 @@ const units = [
     status: "open",
     thumbnail: carousel.filter((e) => e.unity == "stacatarinapool")[14],
     photos: carousel.filter((e) => e.unity == "stacatarinapool"),
-    url: "https://opc-santacatarinapoolandfitness.com/",
+    url: "https://app.ynnovbooking.com/booking/?l=en&apikey=M47SgVH8SkXazFFGd2R1x98kankxHXDP",
     map: "https://goo.gl/maps/ZNARYNg3Xs4rwJjo6",
     address: "Rua Santa Catarina 1219, União de Freguesias do Centro, Porto",
     cityImage: {
@@ -34,7 +34,7 @@ const units = [
       link: "mouzinho/main.jpg",
     },
     photos: carousel.filter((e) => e.unity == "mouzinho"),
-    url: "https://opc-mouzinhodasilveira.com/",
+    url: "https://app.ynnovbooking.com/booking/?l=en&apikey=2IZ7rHodJ2jKozt471bFD3hhai71VCU6",
     map: "https://goo.gl/maps/ZNARYNg3Xs4rwJjo6",
     address:
       "Rua Mouzinho da Silveira, 157 - 167 , União de Freguesias do Centro, Porto",
@@ -54,7 +54,7 @@ const units = [
     status: "open",
     thumbnail: carousel.filter((e) => e.unity == "rosario")[8],
     photos: carousel.filter((e) => e.unity == "rosario"),
-    url: "https://opc-rosario.com/",
+    url: "https://app.ynnovbooking.com/booking/?l=en&apikey=0TutZHDQSIiTmJrF5KLW298Ohx0U3Fu2",
     map: "https://goo.gl/maps/ZNARYNg3Xs4rwJjo6",
     address: "227 Rua do Rosario, União de Freguesias do Centro, Porto",
     cityImage: {
