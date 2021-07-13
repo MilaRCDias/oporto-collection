a125df12818a43e622bcf0c66b779235b0a4f4aa
mila <milarabello.dias@gmail.com>
2021-07-13 21:42:27 +0100

    data file structure

---
+++ data/database.js
@@ -0,0 +1,31 @@
+const d = {
+  opc: {
+    brand_photos: ["link1", "link2"],
+    unities: {
+      mouzinho: {
+        name: "Mouzinho Apartments",
+        description: "Tralalaland",
+        key: "",
+        unity_photo: [],
+        facilities: [],
+        accommodation: [
+          {
+            name: "Apartamento Standard com 1 Quarto",
+            capacity: "",
+            area: 1,
+            bathrooms: "",
+            bed_type: [{ type: "double", quantity: 1 }],
+            rooms: 1,
+            reference: "1.1",
+            description: "",
+            type: "apartment",
+            photos: ["link1", "link2"],
+            facilities: [],
+          },
+        ],
+      },
+      santaCatarina: {},
+      rosario: {},
+    },
+  },
+};
